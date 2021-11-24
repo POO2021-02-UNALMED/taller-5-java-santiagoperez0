9903268bca6f5423b969e0860494501163d25517
santiagoperez0 <jperezpi@unal.edu.co>
2021-11-23 21:56:58 -0500

    sss

--- src/main/java/zooAnimales/Animal.java
+++ src/main/java/zooAnimales/Animal.java
@@ -76,7 +76,7 @@ public class Animal {
     public String toString() {
 
         if (zona != null) {
-            return "Mi nombre es " + nombre + ", tengo una edad de" + edad + ", y mi genero es " + genero + ", la zona en la que me ubico es " + zona.getNombre() + ", en el " + zona.getZoo().getNombre();
+            return "Mi nombre es " + nombre + ", tengo una edad de" + edad + ", y mi genero es " + genero + ", la zona en la que me ubico es " + zona.getNombre() + ", en el " + zona.getZoologico().getNombre();
         } else {
             return "Mi nombre es " + nombre + ", tengo una edad de " + edad + ", habito en "+ habitat +" y mi genero es "+genero ;
         }
